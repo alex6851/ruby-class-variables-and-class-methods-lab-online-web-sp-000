7c97bf7750e7585ab1e5eb54aad30b22e2f272f7
Alexander Baker <alex.baker685@gmail.com>
2019-01-04 22:44:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,9 +34,33 @@ class Song
       result
   end
 
+  # def self.genre_count
+  #   genre_count = {}
+  #   @@genres.each do |genre|
+  #     if genre_count[genre]
+  #       genre_count[genre] += 1 
+  #     else
+  #       genre_count[genre] = 1
+  #     end
+  #   end
+  #   genre_count
+  # end
+
   def self.artist_count
     result = @@artists.each_with_object(Hash.new(0)) { |each_artist,counter| counter[each_artist] += 1 }
     result
   end
+
+  # def self.artist_count
+  #   artist_count = {}
+  #   @@artists.each do |artist|
+  #     if artist_count[artist]
+  #       artist_count[artist] += 1 
+  #     else
+  #       artist_count[artist] = 1
+  #     end
+  #   end
+  #   artist_count
+  # end
 
 end
